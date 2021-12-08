7f98ad49fdb3686358f34791a51b85e222c8a4a1
Janosch Menke <jansoch.menke@googlemail.com>
2021-12-08 16:37:55 +0100

    added showim Funktion

--- utils/utils.py
+++ utils/utils.py
@@ -10,6 +10,21 @@ from matplotlib import pyplot as plt
 
 #%% Plot SVC
 
+def imshow(inp, title=None):
+    """Imshow for Tensor.
+    Credit: PyTorch Tutorial: Transfer Learning
+    """
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)
+
+
 def plot_svc(data, model):
     """
     Credit:  Newbedev  - https://github.com/newbedev-com
